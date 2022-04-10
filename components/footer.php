06e598e6b6a1db72d052114eed6e8f1d3fdc0e6c
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-04-10 17:25:49 +0530

    Updated

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,36 @@
+<section class="container footer-section">
+    <div class="col-md-4 footer-col">
+        <img src="assets/images/logo/Logo.png" alt="">
+        <div class="row-one">
+            <p>Get Free Estimation</p>
+            <p id="email">connectonlyn@onlynus.com</p>
+        </div>
+
+        <div class="footer-call-out">
+            <p>Call</p>
+            <div class="footer-call-out-inner">
+                <img src="assets/images/other/flag.png" alt="">
+                <p>+91 73 88 56 56 81</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 footer-col">
+        <h5>Company</h5>
+        <div class="col-two">
+            <p>About Us</p>
+            <p>Career</p>
+            <p>How to Engage</p>
+            <p>We Work With</p>
+            <p>Testimonials</p>
+            <p>FAQs</p>
+            <p>Policies</p>
+            <p>Sitemap</p>
+            <p>Contact</p>
+        </div>
+    </div>
+</section>
+
 </body>
 
 </html>
