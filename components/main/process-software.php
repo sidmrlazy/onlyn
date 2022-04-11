bc87b5db2d6ef30fac292788a7e6c1c4710f5bb1
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-04-11 09:48:18 +0530

    Updated

---
+++ components/main/process-software.php
@@ -0,0 +1,48 @@
+<section class="container process-section">
+    <h1>We Simplify Software Development Process</h1>
+
+    <div class="process-col">
+        <div class="col-md-3">
+            <div class="process-row">
+                <ion-icon name="disc-outline" class="process-icon"></ion-icon>
+                <div class="dotted-line"></div>
+            </div>
+            <h2>Discover</h2>
+            <p>We shape brands through exploration, applying in-depth research to challenge assumptions at every turn.
+            </p>
+        </div>
+
+        <div class="col-md-3">
+            <div class="process-row">
+                <ion-icon name="brush-outline" class="process-icon"></ion-icon>
+                <div class="dotted-line"></div>
+            </div>
+            <h2>Design</h2>
+            <p>Our design approach is to simplify. We embrace the joy in creating something unique that is easy for end
+                users.
+            </p>
+        </div>
+
+        <div class="col-md-3">
+            <div class="process-row">
+                <ion-icon name="construct-outline" class="process-icon"></ion-icon>
+                <div class="dotted-line"></div>
+            </div>
+            <h2>Build</h2>
+            <p>Using modern technologies, we build with efficiency and skill, creating flexible and scalable
+                business-driven solutions.
+            </p>
+        </div>
+
+        <div class="col-md-3">
+            <div class="process-row">
+                <ion-icon name="paper-plane-outline" class="process-icon"></ion-icon>
+                <div class="dotted-line"></div>
+            </div>
+            <h2>Deliver</h2>
+            <p>We take an iterative approach to both our work and our practice, always looking for ways to improve what
+                we do.
+            </p>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include('components/header.php') ?>
 <?php include('components/navbar.php') ?>
 <?php include('components/main/product-banner.php') ?>
+<?php include('components/main/process-software.php') ?>
 <?php include('components/footer.php') ?>
