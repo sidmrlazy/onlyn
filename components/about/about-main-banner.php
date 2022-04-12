6843f2e337273c71488069d6641370ec49f7bec8
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-04-12 10:13:25 +0530

    Updated About Section

--- components/about/about-main-banner.php
+++ components/about/about-main-banner.php
@@ -1,10 +1,61 @@
 <div class="about-section">
     <div class="container about-section-header">
         <h3>Award-winning Offshore Software Development Company Since 1998</h3>
-        <h1 class="col-md-10 mt-3">Our Treasure Comprises Our People, Vision & Values</h1>
-        <p class="col-md-10 mt-5">IndiaNIC is not only a globally recognized IT company but also a family filled with
+        <h1>Our Treasure Comprises Our People, Vision & Values</h1>
+        <p>IndiaNIC is not only a globally recognized IT company but also a family filled with
             talented experts that
             help
             global brands, enterprises, mid-size businesses or even startups with innovative solutions.</p>
+    </div>
+</div>
+
+<div class="newspaper-section container">
+    <div class="col-md-6 news-row">
+        <h1>Who We Are</h1>
+        <p>IndiaNIC is not an entity, it’s a family that represents togetherness for over two decades of a successful
+            journey. For IndiaNICians, the definition of success is to transcend innovative ideas of people to reality
+            with the help of our tech expertise, this is what we, as a Team, want to be remembered for!</p>
+        <p>Our vision has led IndiaNIC to become a top IT company in India & USA for delivering various industry-led
+            mobility solutions. The goal is to empower clients and businesses by creating new possibilities leveraging
+            the technologies of today and tomorrow with the utmost quality, satisfaction, and transparency.</p>
+    </div>
+
+    <div class="col-md-6 news-row">
+        <h1>What We Do</h1>
+        <p>Our enthusiasm has led us to become a top IT company in India & USA for delivering various industry-led
+            mobility solutions in web and mobile application development domains leveraging futuristic technologies like
+            Internet of Things (IoT), AI-ML, AR-VR, Voice assistants and Voice Skills, DevOps & Cloud computing, etc.
+        </p>
+        <p>We feel empowered with our certified tech experts and our R&D team who have always challenged themselves to
+            help global clientele with a plethora of IT services and solutions. Engagement with our team guarantees our
+            clients to save huge money on project development with faster delivery.</p>
+    </div>
+</div>
+
+<div class="newspaper-section container">
+    <div class="col-md-4 news-row">
+        <h1>Client-centric Approach</h1>
+        <p>For any business, customers are always at the center. Being a leading web and mobile app development company,
+            our definition goes beyond our direct customers. We always start from where you are with your ideas and we
+            think from YOUR end customers' perspectives, their pain areas and devise a solution that solves core
+            problems to benefit your business.</p>
+    </div>
+
+    <div class="col-md-4 news-row">
+        <h1>Effective Collaboration</h1>
+        <p>Communication is the Key. We tend to over-communicate at times. Although sounds overwhelming, but eventually
+            it works wonders for our client business. When our development team collaborates with our clients sitting a
+            thousand miles away, it's apparent that we stay in touch with platforms and mediums that allow
+            effectiveness, transparency, and accuracy.
+        </p>
+    </div>
+
+    <div class="col-md-4 news-row">
+        <h1>Quality Delivered in Time</h1>
+        <p>With over two decades of experience in the offshore IT software development industry, we have a great set of
+            internal frameworks with best-in-class infrastructure that enables us to deliver solutions with superior
+            quality, at all times. All these years, we have learned how to prevent failures and replicate success. We
+            don't just brag about quality. We define and deliver it in time.
+        </p>
     </div>
 </div>
