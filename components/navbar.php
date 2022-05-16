d72b46c12f7d3e8a719339ba68cc5bae8fdfce36
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-05-16 16:09:19 +0530

    Updated

--- components/navbar.php
+++ components/navbar.php
@@ -81,6 +81,13 @@
                 </li>
 
                 <li class="nav-item">
+                    <a class="nav-link" href="consultancy">
+                        <ion-icon name="cafe-outline"></ion-icon>
+                        Consultancy
+                    </a>
+                </li>
+
+                <li class="nav-item">
                     <a class="nav-link" href="methodology">
                         <ion-icon name="color-wand-outline"></ion-icon>
                         Methodology
