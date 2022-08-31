0b4c6e36e7a482bd41576f43d566beec6c19b967
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-08-31 17:02:33 +0530

    modified:   nerdy/assets/js/toasts.js modified:   nerdy/toasts.php

--- nerdy/toasts.php
+++ nerdy/toasts.php
@@ -212,7 +212,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
-                <p>Announcement created!</p>
+                <p>Announcement created! <a href="announcement.php">Click here</a> to go back </p>
             </div>
         </div>
     </div>
