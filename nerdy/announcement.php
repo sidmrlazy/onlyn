df5805c369771c92870f8c117c5d1081017b80d2
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-09-27 13:40:51 +0530

    modified:   nerdy/announcement.php

--- nerdy/announcement.php
+++ nerdy/announcement.php
@@ -25,7 +25,7 @@
                 <label for="floatingSelect">Click here for dropdown menu</label>
             </div>
 
-            <div id="selectClass" class="form-floating mb-3" style="display: none;">
+            <!-- <div id="selectClass" class="form-floating mb-3" style="display: none;">
                 <select name="ann_to_class" class="form-select" id="floatingSelect"
                     aria-label="Floating label select example">
                     <option>Select Class</option>
@@ -43,21 +43,24 @@
                     } ?>
                 </select>
                 <label for="floatingSelect">Click here for dropdown menu</label>
-            </div>
+            </div> -->
 
             <div id="selectIndividualTeacher" class="form-floating mb-3" style="display: none;">
                 <select name="ann_to_teacher" class="form-select" id="floatingSelect"
                     aria-label="Floating label select example">
-                    <!-- <option>Select Individual Teacher</option> -->
                     <?php
-                    $fetch_teacher = "SELECT * FROM users WHERE user_added_by = $session_user_id AND user_type = 3 AND user_status = 1";
+                    $fetch_teacher = "SELECT * FROM users WHERE user_added_by = $session_user_id AND user_status = 1";
                     $fetch_teacher_result = mysqli_query($connection, $fetch_teacher);
                     while ($row = mysqli_fetch_assoc($fetch_teacher_result)) {
                         $teacher_id = $row['user_id'];
                         $user_name = $row['user_name'];
+                        $user_type = $row['user_type'];
+
+                        if ($user_type == 3 || $user_type == 5) {
                     ?>
                     <option value="<?php echo $teacher_id; ?>"><?php echo $user_name; ?></option>
                     <?php
+                        }
                     } ?>
                 </select>
                 <label for="floatingSelect">Click here for dropdown menu</label>
@@ -74,64 +77,55 @@
             </h3>
             <p class="section-desc">View past announcements made by you</p>
         </div>
-
-
-        <?php
-        if (!empty($_SESSION['user_type'])) {
-            $session_user_id = $_SESSION['user_id'];
-        } else {
-            $session_user_id = 0;
-        }
-        $query = "SELECT * FROM announcement WHERE ann_by = $session_user_id ORDER BY ann_id DESC";
-        $result = mysqli_query($connection, $query);
-        while ($row = mysqli_fetch_assoc($result)) {
-            $ann_topic = $row['ann_topic'];
-            $ann_details = $row['ann_details'];
-            $ann_to_type = $row['ann_to_type'];
-            if ($ann_to_type == 1) {
-                $ann_to_type = "Individual Class";
-            } else if ($ann_to_type == 2) {
-                $ann_to_type = "All Classes";
-            } else if ($ann_to_type == 3) {
-                $ann_to_type = "All Teachers";
-            } else if ($ann_to_type == 4) {
-                $ann_to_type = "Individual Teacher";
+        <div class="ann-grid">
+            <?php
+            if (!empty($_SESSION['user_type'])) {
+                $session_user_id = $_SESSION['user_id'];
+            } else {
+                $session_user_id = 0;
             }
-            $ann_to = $row['ann_to'];
-            if ($ann_to == 0) {
-                $ann_to = "Sent to all";
+
+            if (isset($_POST['delete'])) {
+                $ann_id = $_POST['ann_id'];
+                $delete = "DELETE FROM `announcement` WHERE `ann_id` = $ann_id";
+                $del_r = mysqli_query($connection, $delete);
             }
-            $ann_date = $row['ann_date']; ?>
-        <div class="ann-card">
-            <div class="ann-row">
-                <div class="ann-tab">
-                    <p class="ann-label">Announcement for:</p>
-                    <p class="ann-res"><?php echo $ann_to_type; ?></p>
+
+            $query = "SELECT * FROM announcement WHERE ann_by = $session_user_id ORDER BY ann_id DESC";
+            $result = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($result)) {
+                $ann_id = $row['ann_id'];
+                $ann_topic = $row['ann_topic'];
+                $ann_details = $row['ann_details'];
+                $ann_to_type = $row['ann_to_type'];
+                if ($ann_to_type == 1) {
+                    $ann_to_type = "Individual Class";
+                } else if ($ann_to_type == 2) {
+                    $ann_to_type = "All Classes";
+                } else if ($ann_to_type == 3) {
+                    $ann_to_type = "All Teachers";
+                } else if ($ann_to_type == 4) {
+                    $ann_to_type = "Individual Teacher";
+                }
+                $ann_to = $row['ann_to'];
+                if ($ann_to == 0) {
+                    $ann_to = "Sent to all";
+                }
+                $ann_date = $row['ann_date']; ?>
+            <form action="" method="POST" class="ann-box">
+                <input type="text" name="ann_id" value="<?php echo $ann_id ?>" hidden>
+                <div class="ann-inner">
+                    <p class="ann-topic"><?php echo $ann_topic ?></p>
+                    <p class="ann-date"><?php echo $ann_date ?></p>
                 </div>
+                <button type="submit" name="delete" class="del-ann-btn">
+                    <ion-icon name="trash-outline"></ion-icon>
+                </button>
+            </form>
+            <?php
+            } ?>
 
-                <div class="ann-tab">
-                    <p class="ann-label">Announcement to:</p>
-                    <p class="ann-res"><?php echo $ann_to; ?></p>
-                </div>
-
-                <div class="ann-tab">
-                    <p class="ann-label">Date</p>
-                    <p class="ann-res"><?php echo $ann_date; ?></p>
-                </div>
-            </div>
-
-            <div>
-                <p class="ann-label">Topic</p>
-                <p class="ann-res"><?php echo $ann_topic; ?></p>
-            </div>
-
-            <div>
-                <p class="ann-label">Content</p>
-                <p class="ann-res"><?php echo $ann_details; ?></p>
-            </div>
         </div>
-        <?php
-        } ?>
     </div>
 </div>
 <?php include('main/footer.php'); ?>
