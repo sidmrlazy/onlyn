26a025b08964a2724629ed7e2b84b729729759e4
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-09-27 15:50:25 +0530

    modified:   nerdy/api/school-data.php

--- nerdy/api/school-data.php
+++ nerdy/api/school-data.php
@@ -1,13 +1,13 @@
 <?php
 
-include 'connection.php';
+include './connection.php';
 
 // Create connection
-// $connection = new mysqli($host_name, $host_user, $host_password, $host_db);
+$connection = new mysqli($host_name, $host_user, $host_password, $host_db);
 
-// if ($connection->connect_error) {
-//     die("Connection failed: " . $connection->connect_error);
-// }
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
 
 $json = json_decode(file_get_contents('php://input'), true);
 $ProfileData = array();
