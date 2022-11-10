6e42a13d6caf5a69bd613dbe145a12f033285ba2
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-11-10 16:05:41 +0530

    modified:   jaipuriafaizabad/assets/styles.css modified:   jaipuriafaizabad/views/homepage-why-us.php

--- jaipuriafaizabad/views/homepage-why-us.php
+++ jaipuriafaizabad/views/homepage-why-us.php
@@ -3,25 +3,25 @@
         <h2>Why Choose Us?</h2>
         <p>A choice that makes the difference!</p>
     </div>
-    <div class="">
-        <div>
-            <ion-icon name="analytics-outline"></ion-icon>
+    <div class="homepage-why-us-centered">
+        <div class="homepage-why-us-card">
+            <ion-icon class="homepage-why-us-icon" name="analytics-outline"></ion-icon>
             <h3>Highly Experienced</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore
                 magna aliqua. </p>
         </div>
 
-        <div>
-            <ion-icon name="chatbubbles-outline"></ion-icon>
+        <div class="homepage-why-us-card">
+            <ion-icon class="homepage-why-us-icon" name="chatbubbles-outline"></ion-icon>
             <h3>Dedicated Support</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore
                 magna aliqua. </p>
         </div>
 
-        <div>
-            <ion-icon name="library-outline"></ion-icon>
+        <div class="homepage-why-us-card">
+            <ion-icon class="homepage-why-us-icon" name="library-outline"></ion-icon>
             <h3>Question, Quiz & Courses</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore
