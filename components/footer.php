4e37e8ae86441160e773e579f7ead625d8a46942
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-11-23 11:37:54 +0530

    modified:   components/footer.php

--- components/footer.php
+++ components/footer.php
@@ -108,6 +108,10 @@
     </div>
 </section>
 
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-637db4db3de7f646"></script>
+
+
 </body>
 
 </html>
