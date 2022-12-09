42639906b0f19f36c6ba9089d9e3dab346b0d8b9
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-12-09 13:04:09 +0530

    modified:   jp/assets/styles.css modified:   jp/views/homepage-why-us.php

--- jp/views/homepage-why-us.php
+++ jp/views/homepage-why-us.php
@@ -1,31 +1,33 @@
 <div class="homepage-why-us-container">
     <div class="homepage-why-us-header">
-        <h2>Why Choose Us?</h2>
+        <h2>Jaipuria School - Inter School Activities</h2>
         <p>A choice that makes the difference!</p>
     </div>
     <div class="homepage-why-us-centered">
         <div class="homepage-why-us-card">
             <ion-icon class="homepage-why-us-icon" name="analytics-outline"></ion-icon>
-            <h3>Highly Experienced</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore
-                magna aliqua. </p>
+            <h3>Crescendo</h3>
+            <p>Crescendo is a stage where little Jaipurians give wings to their budding talents and take it higher.
+                Crescendo provides a platform for all those who want to showcase their extraordinary talent. Crescendo
+                highlights the development of an individual through various competitions so that they become more
+                confident and ensures a bright future for our country.</p>
         </div>
 
         <div class="homepage-why-us-card">
             <ion-icon class="homepage-why-us-icon" name="chatbubbles-outline"></ion-icon>
-            <h3>Dedicated Support</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore
-                magna aliqua. </p>
+            <h3>Pinnacle</h3>
+            <p>Seth M.R. Jaipuria Schools offer its students a wider stage to learn, grow and excel. Pinnacle, our
+                inter-school sports and cultural event, elevate preparation and performance to a new height. Pinnacle
+                3rd inter-Jaipuria school sports championship was being organized in Lucknow. This sports tournament
+                witnessed participation from around 3500+ students from 25 Jaipuria schools pan-India.</p>
         </div>
 
         <div class="homepage-why-us-card">
             <ion-icon class="homepage-why-us-icon" name="library-outline"></ion-icon>
-            <h3>Question, Quiz & Courses</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore
-                magna aliqua. </p>
+            <h3>Upskill</h3>
+            <p>Quality education is a dire need of the hour. Education that matters; the one that brings a positive
+                change in society and the world at large. To meet this noble objective, we need teachers of high caliber
+                and outlook. The belief that a teacher is the backbone of a society is still practical and relevant.</p>
         </div>
     </div>
 </div>
