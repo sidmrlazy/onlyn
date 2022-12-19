5875a9711367d5a2d3af7bfd3538e79fe40505fb
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-12-19 16:15:45 +0530

    modified:   rgs-new/assets/css/styles.css modified:   rgs-new/components/home/carousel.php modified:   rgs-new/includes/header.php modified:   rgs-new/includes/navbar.php modified:   rgs-new/index.php

--- rgs-new/index.php
+++ rgs-new/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = "RGS College of Pharmacy";
+$title = "Top College of Pharmacy in Lucknow | RGS College of Pharmacy";
 include('includes/header.php') ?>
 <?php include('includes/navbar.php') ?>
 <?php include('components/home/carousel.php') ?>
