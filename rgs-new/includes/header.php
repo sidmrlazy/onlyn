4ec6392e1b304aae11e3a1c78da4149e1025bc3b
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-12-19 16:24:30 +0530

    modified:   rgs-new/assets/css/styles.css modified:   rgs-new/includes/header.php

--- rgs-new/includes/header.php
+++ rgs-new/includes/header.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="">
     <meta name="description" content="">
 
-    <!-- =========== BOOTSTRAP =========== -->
+    <!-- =========== BOOTSTRAP v5.2 =========== -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
