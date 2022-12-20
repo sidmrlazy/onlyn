e62e592c317e2aa8e051f9eca46c9db3efda58a9
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-12-20 14:11:00 +0530

    new file:   jp/assets/images/svg/banner-1.svg new file:   jp/assets/images/svg/banner-2.svg modified:   jp/views/common/admission-banner.php modified:   jp/views/homescreen-banner.php

--- jp/views/common/admission-banner.php
+++ jp/views/common/admission-banner.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
     <div class="admission-page-banner">
-        <img src="assets/images/svg/admission-banner.svg" alt="">
+        <img src="assets/images/svg/banner-2.svg" alt="">
     </div>
 
     <div class="admission-nav-bar">
--- jp/views/homescreen-banner.php
+++ jp/views/homescreen-banner.php
@@ -1,5 +1,5 @@
 <div class="home-screen-banner-container">
-    <img src="assets/images/banner-1.png" alt="">
+    <img src="assets/images/svg/banner-1.svg" alt="">
     <a href="index.php#home-page-brief" class="homepage-chevron-container">
         <ion-icon name="chevron-down-circle-outline"
             class="homepage-chevron animate__animated animate__headShake animate__slower animate__infinite"></ion-icon>
