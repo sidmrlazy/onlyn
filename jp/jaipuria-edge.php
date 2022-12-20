46c16d5b0e010156bbd2f0349ca91b52fca560fe
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-12-20 20:01:48 +0530

    new file:   jp/assets/images/svg/banner-3.svg modified:   jp/jaipuria-edge.php new file:   jp/views/common/jaipuria-edge-banner.php modified:   rgs-new/assets/css/styles.css

--- jp/jaipuria-edge.php
+++ jp/jaipuria-edge.php
@@ -2,5 +2,5 @@
 $title = "Jaipuria Edge | ";
 include('includes/header.php') ?>
 <?php include('includes/navigation.php') ?>
-
+<?php include('views/common/jaipuria-edge-banner.php') ?>
 <?php include('includes/footer.php') ?>
---
+++ jp/views/common/jaipuria-edge-banner.php
@@ -0,0 +1,5 @@
+<div class="container-fluid">
+    <div class="admission-page-banner">
+        <img src="assets/images/svg/banner-3.svg" alt="">
+    </div>
+</div>
