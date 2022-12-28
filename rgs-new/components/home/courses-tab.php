48845c73ceb8c9521316720ff944875bb1c36b2b
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-12-28 11:24:54 +0530

    modified:   rgs-new/assets/css/styles.css modified:   rgs-new/components/home/courses-tab.php

--- rgs-new/components/home/courses-tab.php
+++ rgs-new/components/home/courses-tab.php
@@ -1,35 +1,33 @@
-<div class="container mb-5">
-    <div class="home-page-courses-tab-section">
-        <div class="col-md-6">
-            <div class="course-tab-inner-row">
-                <div class="courses-inner-card" id="blue">
-                    <h3>Course</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
-                </div>
-
-                <div class="courses-inner-card" id="dark-blue">
-                    <h3>Course</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
-                </div>
+<div class="home-page-courses-tab-section">
+    <div class="col-md-6">
+        <div class="course-tab-inner-row">
+            <div class="courses-inner-card" id="blue">
+                <h3>Course</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
             </div>
-            <div class="course-tab-inner-row">
-                <div class="courses-inner-card" id="grey">
-                    <h3>Course</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
-                </div>
 
-                <div class="courses-inner-card" id="yellow">
-                    <h3>Course</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
-                </div>
+            <div class="courses-inner-card" id="dark-blue">
+                <h3>Course</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
             </div>
         </div>
-
-        <div class="col-md-6 mobile-view">
-            <div class="home-page-brief-lottie">
-                <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_ikvz7qhc.json"
-                    background="transparent" speed="1" loop autoplay></lottie-player>
+        <div class="course-tab-inner-row">
+            <div class="courses-inner-card" id="grey">
+                <h3>Course</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
             </div>
+
+            <div class="courses-inner-card" id="yellow">
+                <h3>Course</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nulla nobis sequi.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6 mobile-view">
+        <div class="home-page-brief-lottie">
+            <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_ikvz7qhc.json" background="transparent"
+                speed="1" loop autoplay></lottie-player>
         </div>
     </div>
 </div>
