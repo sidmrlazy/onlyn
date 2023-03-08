2ba176deef954358febad460f2e67c13aa74607c
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-03-08 10:09:22 +0530

    modified:   components/footer.php

--- components/footer.php
+++ components/footer.php
@@ -109,8 +109,9 @@
 </section>
 
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-637db4db3de7f646"></script>
-
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-64080f9e5b2ccfd7"></script>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<div class="addthis_inline_follow_toolbox"></div>
 
 </body>
 
