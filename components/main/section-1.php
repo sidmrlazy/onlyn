93319c06d2bf5776c9227c94f1e450dbe8402d76
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-04-27 09:36:26 +0530

    modified:   assets/style.css modified:   components/header.php deleted:    components/main/product-banner.php new file:   components/main/section-1.php renamed:    components/main/process-software.php -> components/main/section-2.php renamed:    components/main/middle-section.php -> components/main/section-3.php renamed:    components/main/tech-work.php -> components/main/section-4.php modified:   index.php

---
+++ components/main/section-1.php
@@ -0,0 +1,15 @@
+<div class="product-banner">
+    <div class="col-md-4">
+        <h1>Empower Your Business with Custom Tech Solutions from India's Top IT Company</h1>
+        <p>Since 2017, our software development company in India has been delivering innovative solutions for global
+            businesses. As one of the best software companies in India, we specialize in custom software development,
+            web development, mobile app development, ecommerce development, and digital marketing services. With a
+            design-focused approach and streamlined project execution processes, we deliver the right solutions for
+            exponential business growth. Partner with us, a top IT company in India, and take your business to the next
+            level.</p>
+    </div>
+    <div class="col-md-8 lottie-holder">
+        <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_ot09abmx.json" background="transparent"
+            speed="1" class="lottie" loop autoplay></lottie-player>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('components/header.php') ?>
 <?php include('components/navbar.php') ?>
-<?php include('components/main/product-banner.php') ?>
-<?php include('components/main/process-software.php') ?>
-<?php include('components/main/middle-section.php') ?>
-<?php include('components/main/tech-work.php') ?>
+<?php include('components/main/section-1.php') ?>
+<?php include('components/main/section-2.php') ?>
+<?php include('components/main/section-3.php') ?>
+<?php include('components/main/section-4.php') ?>
 <?php include('components/footer.php') ?>
