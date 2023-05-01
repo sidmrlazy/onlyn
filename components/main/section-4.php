1d241913ce8c1f3283f70298d73b287aee2d8978
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-05-01 09:30:50 +0530

    modified:   components/main/section-4.php

--- components/main/section-4.php
+++ components/main/section-4.php
@@ -1,115 +1,119 @@
-<div class="tech-work">
+<div class="container">
+    <div class="tech-work">
 
-    <h3>Technolgies we work with</h1>
-        <!-- =================  Navigation  ===================== -->
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item tab-item" role="presentation">
-                <button class="nav-link static-nav-link active" id="mobile-tab" data-bs-toggle="tab"
-                    data-bs-target="#mobile" type="button" role="tab" aria-controls="mobile"
-                    aria-selected="true">Mobile</button>
-            </li>
-            <li class="nav-item tab-item" role="presentation">
-                <button class="nav-link static-nav-link" id="frontend-tab" data-bs-toggle="tab"
-                    data-bs-target="#frontend" type="button" role="tab" aria-controls="frontend"
-                    aria-selected="false">Frontend</button>
-            </li>
-            <li class="nav-item tab-item" role="presentation">
-                <button class="nav-link static-nav-link" id="database-tab" data-bs-toggle="tab"
-                    data-bs-target="#database" type="button" role="tab" aria-controls="database"
-                    aria-selected="false">Database</button>
-            </li>
-            <li class="nav-item tab-item" role="presentation">
-                <button class="nav-link static-nav-link" id="backend-tab" data-bs-toggle="tab" data-bs-target="#backend"
-                    type="button" role="tab" aria-controls="backend" aria-selected="false">Backend</button>
-            </li>
-        </ul>
+        <h3>Technolgies we work with</h1>
+            <!-- =================  Navigation  ===================== -->
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item tab-item" role="presentation">
+                    <button class="nav-link static-nav-link active" id="mobile-tab" data-bs-toggle="tab"
+                        data-bs-target="#mobile" type="button" role="tab" aria-controls="mobile"
+                        aria-selected="true">Mobile</button>
+                </li>
+                <li class="nav-item tab-item" role="presentation">
+                    <button class="nav-link static-nav-link" id="frontend-tab" data-bs-toggle="tab"
+                        data-bs-target="#frontend" type="button" role="tab" aria-controls="frontend"
+                        aria-selected="false">Frontend</button>
+                </li>
+                <li class="nav-item tab-item" role="presentation">
+                    <button class="nav-link static-nav-link" id="database-tab" data-bs-toggle="tab"
+                        data-bs-target="#database" type="button" role="tab" aria-controls="database"
+                        aria-selected="false">Database</button>
+                </li>
+                <li class="nav-item tab-item" role="presentation">
+                    <button class="nav-link static-nav-link" id="backend-tab" data-bs-toggle="tab"
+                        data-bs-target="#backend" type="button" role="tab" aria-controls="backend"
+                        aria-selected="false">Backend</button>
+                </li>
+            </ul>
 
 
-        <div class="tab-content" id="myTabContent">
-            <!-- ============= Mobile ============= -->
-            <div class="tab-pane fade show active" id="mobile" role="tabpanel" aria-labelledby="mobile-tab">
-                <div class="accordion-body">
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/apple.png" alt="mobile app development on ios">
-                        <p>iOS</p>
+            <div class="tab-content" id="myTabContent">
+                <!-- ============= Mobile ============= -->
+                <div class="tab-pane fade show active" id="mobile" role="tabpanel" aria-labelledby="mobile-tab">
+                    <div class="accordion-body">
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/apple.png" alt="mobile app development on ios">
+                            <p>iOS</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/android.png" alt="mobile app development on android">
+                            <p>Android</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/react-native.png"
+                                alt="mobile app development using React Native">
+                            <p>React Native</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/swift.png" alt="mobile app development using Swift">
+                            <p>Swift</p>
+                        </div>
                     </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/android.png" alt="mobile app development on android">
-                        <p>Android</p>
+                </div>
+
+                <!-- ============= Frontend ============= -->
+                <div class="tab-pane fade" id="frontend" role="tabpanel" aria-labelledby="frontend-tab">
+                    <div class="accordion-body">
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/html.png" alt="website development with html5">
+                            <p>HTML5</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/css.png" alt="website development using css">
+                            <p>CSS</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/react-native.png" alt="website development on react.js">
+                            <p>React JS</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/typescript.png" alt="website development with TypeScript">
+                            <p>TypeScript </p>
+                        </div>
                     </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/react-native.png" alt="mobile app development using React Native">
-                        <p>React Native</p>
+                </div>
+
+                <!-- ============= Database ============= -->
+                <div class="tab-pane fade" id="database" role="tabpanel" aria-labelledby="database-tab">
+                    <div class="accordion-body">
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/sql.png" alt="website development using MySQL">
+                            <p>MySQL</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/postSql.png" alt="website development using PostgreSQL">
+                            <p>PostgreSQL</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/firebase.png" alt="website development using firebase">
+                            <p>Firebase</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/mongo.png" alt="website development using MongoDB">
+                            <p>MongoDB</p>
+                        </div>
                     </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/swift.png" alt="mobile app development using Swift">
-                        <p>Swift</p>
+                </div>
+                <div class="tab-pane fade" id="backend" role="tabpanel" aria-labelledby="backend-tab">
+                    <div class="accordion-body">
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/php.png" alt="website development on PHP">
+                            <p>PHP</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/java.png" alt="website development using JAVA">
+                            <p>Java</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/python.png" alt="website development using Python">
+                            <p>Python</p>
+                        </div>
+                        <div class="col-md-3 custom-card">
+                            <img src="assets/images/icons/node.png" alt="webiste development using Node">
+                            <p>Node</p>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <!-- ============= Frontend ============= -->
-            <div class="tab-pane fade" id="frontend" role="tabpanel" aria-labelledby="frontend-tab">
-                <div class="accordion-body">
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/html.png" alt="website development with html5">
-                        <p>HTML5</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/css.png" alt="website development using css">
-                        <p>CSS</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/react-native.png" alt="website development on react.js">
-                        <p>React JS</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/typescript.png" alt="website development with TypeScript">
-                        <p>TypeScript </p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- ============= Database ============= -->
-            <div class="tab-pane fade" id="database" role="tabpanel" aria-labelledby="database-tab">
-                <div class="accordion-body">
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/sql.png" alt="website development using MySQL">
-                        <p>MySQL</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/postSql.png" alt="website development using PostgreSQL">
-                        <p>PostgreSQL</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/firebase.png" alt="website development using firebase">
-                        <p>Firebase</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/mongo.png" alt="website development using MongoDB">
-                        <p>MongoDB</p>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="backend" role="tabpanel" aria-labelledby="backend-tab">
-                <div class="accordion-body">
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/php.png" alt="website development on PHP">
-                        <p>PHP</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/java.png" alt="website development using JAVA">
-                        <p>Java</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/python.png" alt="website development using Python">
-                        <p>Python</p>
-                    </div>
-                    <div class="col-md-3 custom-card">
-                        <img src="assets/images/icons/node.png" alt="webiste development using Node">
-                        <p>Node</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
 </div>
