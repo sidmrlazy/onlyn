f13029f565ac08293b17392a281ef602418f1c48
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-03 10:11:13 +0530

    new file:   project/admin/assets-admin/press/section-2-img-carousel-img-2.webp modified:   project/admin/main/db.php modified:   project/assets/styles.css modified:   project/components/press-release/section-2-eng.php

--- project/components/press-release/section-2-eng.php
+++ project/components/press-release/section-2-eng.php
@@ -12,17 +12,17 @@
             $press_release_link = $row['press_release_link'];
             $press_release_date = $row['press_release_date'];
         ?>
-            <form action="press-release-solo.php" method="POST" class="press-release-card">
-                <input type="text" name="press_release_id" value="<?php echo $press_release_id ?>" hidden>
-                <img src="<?php echo $press_release_img ?>" alt="">
-                <div class="press-release-card-content-holder">
-                    <h2><?php echo $press_release_title  ?></h2>
-                    <p class="press-release-content"><?php echo $press_release_content ?></p>
+        <form action="press-release-solo.php" method="POST" class="press-release-card">
+            <input type="text" name="press_release_id" value="<?php echo $press_release_id ?>" hidden>
+            <img src="<?php echo $press_release_img ?>" alt="">
+            <div class="press-release-card-content-holder">
+                <h2><?php echo $press_release_title  ?></h2>
+                <p class="press-release-content"><?php echo $press_release_content ?></p>
 
-                    <p><?php echo date('d M Y', strtotime($press_release_date))  ?></p>
-                    <button type="submit" name="read" class="press-release-btn">Read Full Story</button>
-                </div>
-            </form>
+                <p><?php echo date('d M Y', strtotime($press_release_date))  ?></p>
+                <button type="submit" name="read" class="press-release-btn">Read Full Story</button>
+            </div>
+        </form>
         <?php } ?>
     </div>
 </div>
