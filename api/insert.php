0c62e7dd4eb654e8dfaa76a0c116996717576937
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-10-04 17:05:06 +0530

    modified:   api/add-shop.php modified:   api/insert.php modified:   api/login.php

--- api/insert.php
+++ api/insert.php
@@ -6,9 +6,9 @@
 
 // Production
 $servername = "localhost";
-$username = "u976956619_onlyn";
-$database = "u976956619_onlyn";
-$password = "Sid12asthana";
+$username = "u976956619_nerdy";
+$database = "u976956619_nerdy";
+$password = "Darthvader@order66";
 
 // Validate Connection
 $connection = new mysqli($servername, $username, $password, $database);
