1dbccea1500688e6e3e0d73f6b1f91df004aa7ef
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2024-01-18 14:06:03 +0530

    new file:   adiraerp/assets/Logo-Half.png modified:   adiraerp/assets/styles.css modified:   adiraerp/components/footer.php

--- adiraerp/components/footer.php
+++ adiraerp/components/footer.php
@@ -1,3 +1,11 @@
+<!-- ============ COMPANY BRANDING ============ -->
+<div class="footer">
+    <div class="company-footer">
+        <p>&copy; Designed & Developed by <a href="https://onlynus.com/" target="_blank"><img src="assets/Logo-Half.png" alt=""></a></p>
+        <p class="footer-bottom">(An I.T. & Branding Venture by Siddharth Asthana) </p>
+    </div>
+</div>
+
 <!-- ============ BOOTSTRAP ============ -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
