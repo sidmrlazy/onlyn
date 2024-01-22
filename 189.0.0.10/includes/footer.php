43ad0be862359f22d6254c604ebdc41c5feb1bb2
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2024-01-22 13:55:50 +0530

    modified:   189.0.0.10/includes/footer.php

--- 189.0.0.10/includes/footer.php
+++ 189.0.0.10/includes/footer.php
@@ -1,7 +1,7 @@
-<div class="developer-footer">
+<!-- <div class="developer-footer">
     <p>&copy; Designed & Developed by <a href="https://onlynus.com/" target="_blank"><img src="assets/Logo-Half.png" alt=""></a></p>
     <p class="footer-bottom">(An I.T. & Branding Venture by Siddharth Asthana) </p>
-</div>
+</div> -->
 
 <!-- ============ BOOTSTRAP ============ -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
